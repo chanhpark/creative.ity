9035c7c1df6cb690b1845dc814b54a082dbc9dd4
chanhpark <chanhyun.park1211@gmail.com>
2015-01-09 22:33:44 -0500

    user comments on a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,15 +3,29 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id]) #finding post
-    @comment = Comment.create(params[:comment].permit(:content)) #finding the comment with the params
-    @comment.user_id = current_user.id #assigning the user_id to current user
-    @comment.post_id = @post.id #assigning the post_id from comments to the id from posts table
+    @comment = Comment.find_or_initialize_by(post: @post, user: current_user)
+    # @comment.user_id = current_user.id #assigning the user_id to current user
+    # @comment.post_id = @post.id #assigning the post_id from comments to the id from posts table
 
-    if @comment.save
-      redirect_to post_path(@post)
+    if @comment.new_record?
+      @comment.attributes = comment_params
+      if @comment.save
+        redirect_to post_path(@post),
+        notice: "Successfully added your comment"
+      else
+        render 'new'
+      end
     else
-      render 'new'
+    redirect_to post_path(@post),
+    notice: "You have already commented on this post"
     end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+    # @comment = Comment.create(params[:comment].permit(:content)) #finding the comment with the params
   end
 
 end
