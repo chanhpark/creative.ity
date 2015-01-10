a6fb83055ce9cfe0fc344f08fdde3369fb125fab
chanhpark <chanhyun.park1211@gmail.com>
2015-01-09 23:32:06 -0500

    fixed hound errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+  resources :posts, only: [:index,
+                          :show,
+                          :new,
+                          :create,
+                          :edit,
+                          :update,
+                          :destroy] do
     member do
       get "like", to: "posts#upvote"
       get "dislike", to: "posts#downvote"
     end
-    resources :comments, only: [:create, :edit, :update, :destroy]
+    resources :comments, only: [:create,
+                               :edit,
+                               :update,
+                               :destroy]
   end
   root 'posts#index'
 end
