e7b54f23765c7a6f7ae8640977c5ee17b69e30ee
chanhpark <chanhyun.park1211@gmail.com>
2015-01-09 22:37:36 -0500

    fix hound errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id]) #finding post
     @comment = Comment.find_or_initialize_by(post: @post, user: current_user)
     # @comment.user_id = current_user.id #assigning the user_id to current user
-    # @comment.post_id = @post.id #assigning the post_id from comments to the id from posts table
+    # @comment.post_id = @post.id #assigning the post_id from comments
+    # to the id from posts table
 
     if @comment.new_record?
       @comment.attributes = comment_params
@@ -13,11 +14,11 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post),
         notice: "Successfully added your comment"
       else
-        render 'new'
+        render "new"
       end
     else
-    redirect_to post_path(@post),
-    notice: "You have already commented on this post"
+      redirect_to post_path(@post),
+      notice: "You have already commented on this post"
     end
   end
 
@@ -25,7 +26,7 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:content)
-    # @comment = Comment.create(params[:comment].permit(:content)) #finding the comment with the params
+    # @comment = Comment.create(params[:comment].permit(:content))
+    # finding the comment with the params
   end
-
 end
