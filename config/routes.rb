af7f5c69ba7dc2a92aa59131a553ea28142040c0
chanhpark <chanhyun.park1211@gmail.com>
2015-01-09 23:48:41 -0500

    styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts, only: [:index,
-                          :show,
-                          :new,
-                          :create,
-                          :edit,
-                          :update,
-                          :destroy] do
+  resources :posts do
     member do
       get "like", to: "posts#upvote"
       get "dislike", to: "posts#downvote"
     end
-    resources :comments, only: [:create,
-                               :edit,
-                               :update,
-                               :destroy]
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
   root 'posts#index'
 end
